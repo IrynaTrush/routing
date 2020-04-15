0d4e7ba911bd10cf20646ce8741ffb6496111ffd
Iryna <irynka1071994@gmail.com>
2020-04-15 23:07:13 +0300

    Updates

---
+++ precache-manifest.2591d7ea5542f0e54490df6882705892.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d03c21254e5457b78ce9ef24f049329",
+    "url": "/routing/index.html"
+  },
+  {
+    "revision": "d0d7a36b1906554a7347",
+    "url": "/routing/static/css/main.1d08c78c.chunk.css"
+  },
+  {
+    "revision": "75c214d447f69aa2cebf",
+    "url": "/routing/static/js/2.acb1ede0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/routing/static/js/2.acb1ede0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d7a36b1906554a7347",
+    "url": "/routing/static/js/main.d5e8ede4.chunk.js"
+  },
+  {
+    "revision": "b0cc99534389166a14c8",
+    "url": "/routing/static/js/runtime-main.faa86dc2.js"
+  },
+  {
+    "revision": "6bfe3a3d8b9d7d4095fe094681af5322",
+    "url": "/routing/static/media/53519498-easter-eggs-and-cute-bunny-in-green-grass-festive-decoration-happy-easter-.6bfe3a3d.jpg"
+  }
+]);
